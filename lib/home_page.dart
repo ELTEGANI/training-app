4a88fe11119a5570c4fe1a5c88a91d6e1ddfe29e
ELTEGANI MOHAMED <elteganimohamed@ELTEGANIs-MacBook-Air.local>
2022-06-29 17:36:07 +0400

    add two cards

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -87,16 +87,151 @@ class _HomePageState extends State<HomePage> {
                     )
                   ]
                 ),
-                child:Column(
-                  crossAxisAlignment:CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                        "Next Workout",
-                         style: TextStyle(
-                           fontSize:16,
-                           color:color.AppColor.homePageContainerTextSmall
-                         ),
+                child:Container(
+                  padding:const EdgeInsets.only(left:20,top:25,right:20),
+                    child:Column(
+                      crossAxisAlignment:CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Next Workout",
+                          style: TextStyle(
+                              fontSize:16,
+                              color:color.AppColor.homePageContainerTextSmall
+                          ),
+                        ),
+                        SizedBox(height:5,),
+                        Text(
+                          "Legs Toning",
+                          style: TextStyle(
+                              fontSize:25,
+                              color:color.AppColor.homePageContainerTextSmall
+                          ),
+                        ),
+                        SizedBox(height:5,),
+                        Text(
+                          "and Glutes Workout",
+                          style: TextStyle(
+                              fontSize:25,
+                              color:color.AppColor.homePageContainerTextSmall
+                          ),
+                        ),
+                        SizedBox(height:25,),
+                        Row(
+                          crossAxisAlignment:CrossAxisAlignment.end,
+                          children: [
+                            Row(
+                              children: [
+                              Icon(Icons.timer,size:20,color:color.AppColor.homePageContainerTextSmall),
+                              SizedBox(width:10,),
+                              Text(
+                                "60 min",
+                                style: TextStyle(
+                                    fontSize:15,
+                                    color:color.AppColor.homePageContainerTextSmall
+                                ),
+                              ),
+                            ],
+                            ),
+                            Expanded(child: Container()),
+                            Container(
+                              decoration:BoxDecoration(
+                                borderRadius:BorderRadius.circular(60),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color:color.AppColor.gradientFirst,
+                                    blurRadius:10,
+                                    offset:Offset(4,8)
+                                  )
+                                ]
+                              ),
+                              child:Icon(
+                                  Icons.play_circle_fill,
+                                  size:60,
+                                  color:color.AppColor.homePageContainerTextSmall
+                              ),
+                            )
+                          ],
+                        ),
+                      ],
                     )
+                ),
+              ),
+              SizedBox(height:5,),
+              Container(
+                height:180,
+                width:MediaQuery.of(context).size.width,
+                child:Stack(
+                  children: [
+                   Container(
+                       width:MediaQuery.of(context).size.width,
+                       margin:const EdgeInsets.only(top:30),
+                       height:120,
+                       decoration:BoxDecoration(
+                         borderRadius:BorderRadius.circular(20),
+                         image:DecorationImage(
+                           image:AssetImage(
+                             "assets/card.jpg"
+                           ),
+                           fit:BoxFit.fill
+                         ),
+                         boxShadow:[
+                           BoxShadow(
+                             blurRadius:40,
+                             offset:Offset(8,10),
+                             color:color.AppColor.gradientSecond.withOpacity(0.3)
+                           ),
+                           BoxShadow(
+                               blurRadius:10,
+                               offset:Offset(-1,-5),
+                               color:color.AppColor.gradientSecond.withOpacity(0.3)
+                           ),
+                         ]
+                       ),
+                   ),
+                   Container(
+                     height:200,
+                     width:MediaQuery.of(context).size.width,
+                       margin: const EdgeInsets.only(right:200,bottom:30),
+                       decoration:BoxDecoration(
+                           borderRadius:BorderRadius.circular(20),
+                           image:DecorationImage(
+                               image:AssetImage(
+                                   "assets/figure.png"
+                               ),
+                           ),
+                       )
+                   ),
+                   Container(
+                     width:double.maxFinite,
+                     height:100,
+                     margin:const EdgeInsets.only(left:150,top:50),
+                     child:Column(
+                       crossAxisAlignment:CrossAxisAlignment.start,
+                       children: [
+                         Text(
+                           "You are doing great",
+                            style:TextStyle(
+                              fontWeight:FontWeight.bold,
+                              color:color.AppColor.homePageDetail
+                            ),
+                         ),
+                         SizedBox(height:10,),
+                         RichText(text: TextSpan(
+                           text: "Keep it Up \n",
+                           style:TextStyle(
+                             color:color.AppColor.homePagePlanColor,
+                             fontSize:16
+                           ),
+                           children:[
+                             TextSpan(
+                               text:"stick to your plan"
+                             )
+                           ]
+                           )
+                         )
+                       ],
+                     ),
+                   )
                   ],
                 ),
               )
