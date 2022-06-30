233b6064a3f19f695c109ca92d5cfbd403df5e21
ELTEGANI MOHAMED <elteganimohamed@ELTEGANIs-MacBook-Air.local>
2022-06-30 20:35:16 +0400

    add details page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:testing/home_page.dart';
+import 'package:testing/video_info.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: VideoInfo(),
     );
   }
 }
